57c7176b060e99faae3750f7fc5278b1cf4794af
unknown <laseis123@gmail.com>
2023-03-07 18:14:12 -0300

    stringToNumber

---
+++ FirstProject/OB-5/StringToNumber.js
@@ -0,0 +1,15 @@
+// Como convertir los string a valores numericos
+let numero = '5.89'
+let num2 = 17.7
+let num3 = 4
+
+console.log(typeof numero)
+console.log(numero + num2) //Error de concepto
+console.log(Number(numero) + num2) 
+
+console.log(parseInt(numero)) // Si o si sea un numero ENTERO
+console.log(parseFloat(numero)) // Si o si sea un numero FLOTANTE
+
+console.log(parseFloat(num3))
+console.log(parseInt(num3))
+

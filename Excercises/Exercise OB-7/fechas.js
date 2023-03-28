f420483d687503322443b8577bd5d861b4697275
Mateo Bodini <83024999+mateoBodiniARG@users.noreply.github.com>
2023-03-27 22:43:07 -0300

    Update fechas.js

--- Excercises/Exercise OB-7/fechas.js
+++ Excercises/Exercise OB-7/fechas.js
@@ -11,11 +11,12 @@ const masGrande = fecha > fechaNac
 
 
 // - Una variable que contenga el día de tu nacimiento
-console.log(fechaNac.getDate())
+const diaNac = fechaNac.getDate()
+
 
 // - Una variable que contenga el mes de tu nacimiento (recuerda que Enero es mes 0)
-console.log(fechaNac.getMonth()) + 1
+const mesNac = fechaNac.getMonth() + 1
 
 
 // - Una variable que contenga el año de tu nacimiento (con 4 dígitos)
-console.log(fechaNac.getFullYear())
+const yearNac = fechaNac.getFullYear()  

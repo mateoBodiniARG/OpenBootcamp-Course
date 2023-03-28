b46a38f94b13d5df62997daf1991f8f3dbc5e861
unknown <laseis123@gmail.com>
2023-03-27 22:38:03 -0300

    Exercise 7 3/3 resolved

---
+++ Excercises/Exercise OB-7/fechas.js
@@ -0,0 +1,21 @@
+// - La fecha de hoy
+const fecha = new Date()
+
+
+// - La fecha de mi nacimiento
+const fechaNac = new Date(2002, 9, 23)
+
+
+// - Un variable que indique si hoy es más tarde (>) que la fecha de tu nacimiento
+const masGrande = fecha > fechaNac
+
+
+// - Una variable que contenga el día de tu nacimiento
+console.log(fechaNac.getDate())
+
+// - Una variable que contenga el mes de tu nacimiento (recuerda que Enero es mes 0)
+console.log(fechaNac.getMonth())
+
+
+// - Una variable que contenga el año de tu nacimiento (con 4 dígitos)
+console.log(fechaNac.getFullYear())

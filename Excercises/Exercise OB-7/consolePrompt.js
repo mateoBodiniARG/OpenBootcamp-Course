a914425225d12edc06cf80a820970c918a3a70f0
unknown <laseis123@gmail.com>
2023-03-27 22:52:07 -0300

    Module 7 finish

---
+++ Excercises/Exercise OB-7/consolePrompt.js
@@ -0,0 +1,8 @@
+// - Abre una nueva ventana "about:blank" en Google Chrome
+
+// - Abre la consola de desarrollador de Google Chrome (F12)
+
+// - Pregunta al usuario cuál es su edad y almacénala en una variable llamada edad
+
+
+const edad = prompt("¿Cuántos años tienes?")
